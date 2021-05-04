4d130c1e21918e11d1bf359fe42325a395014e44
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-04 13:47:56 +0200

    Add tabs to view, make Kits tab active properly

---
+++ app/overrides/add_product_tab.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Deface::Override.new(virtual_path: "spree/admin/shared/_product_tabs",
+                     name: "configurable_kits_admin_product_tabs",
+                     insert_bottom: "[data-hook='admin_product_tabs']",
+                     partial: "solidus_configurable_kits/shared/configurable_kits_product_tab",
+                     disabled: false)
