56345718853078d3c752bc2442467a8ef0eded59
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-05 10:38:14 +0200

    Replace the entire cart form
    I need to use a different controller for adding kits to the cart than
    the normal orders controller. I can only accomplish this by replacing
    the entire cart form with a "kit" form.

--- app/overrides/add_to_cart_additions.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/products/_cart_form",
-  name: "configurable_kits_add_to_cart_additions",
-  insert_top: "[data-hook='inside_product_cart_form']",
-  partial: "solidus_configurable_kits/shared/configurable_kits_add_to_cart_additions",
-  disabled: false
-)
---
+++ app/overrides/replace_cart_form.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Deface::Override.new(
+  virtual_path: "spree/products/show",
+  name: "configurable_kits_kit_form",
+  surround: "[data-hook='cart_form']",
+  partial: "solidus_configurable_kits/shared/kit_cart_form",
+  disabled: false
+)
