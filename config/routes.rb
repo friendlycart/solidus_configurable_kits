c87e34dadc68d9862daedc8cc286e86f5663697d
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-05 11:00:03 +0200

    Add a custom orders#populate_kits action
    This action is copy-pasted from Solidus and for now does the same thing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 SolidusConfigurableKits::Engine.routes.draw do
+  resources :orders, only: [] do
+    post :populate_kit, on: :collection
+  end
+
   namespace :admin do
     resources :products, only: [] do
       resources :kit_requirements, controller: :kit_requirements
