7d906f77cb7430c4637269c446890ae5b22edeaf
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-06 21:26:49 +0200

    Make Migration work in Rails 5.2

--- db/migrate/20210506145818_add_kit_item_to_spree_prices.rb
+++ db/migrate/20210506145818_add_kit_item_to_spree_prices.rb
@@ -1,4 +1,4 @@
-class AddKitItemToSpreePrices < ActiveRecord::Migration[6.1]
+class AddKitItemToSpreePrices < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_prices, :kit_item, :boolean, default: false, null: false
   end
