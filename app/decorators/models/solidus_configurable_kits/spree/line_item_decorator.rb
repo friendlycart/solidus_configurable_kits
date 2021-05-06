38a3b20d56d9184832b18519d37ca114965efc25
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-06 14:22:50 +0200

    Update Kit Item quantities along with Kit quantity

--- app/decorators/models/solidus_configurable_kits/spree/line_item_decorator.rb
+++ app/decorators/models/solidus_configurable_kits/spree/line_item_decorator.rb
@@ -9,10 +9,23 @@ module SolidusConfigurableKits
                       foreign_key: :kit_item_id,
                       dependent: :destroy
         base.belongs_to :kit, class_name: "Spree::LineItem", foreign_key: :kit_item_id, optional: true
+        base.before_update :update_kit_item_quantities
       end
 
       def kit_item?
         kit.present?
+      end
+
+      private
+
+      def update_kit_item_quantities
+        if quantity_changed?
+          kit_items.each do |kit_item|
+            single_kit_item_quantity = kit_item.quantity / quantity_was
+            kit_item.quantity = single_kit_item_quantity * quantity
+            kit_item.save
+          end
+        end
       end
 
       ::Spree::LineItem.prepend self
