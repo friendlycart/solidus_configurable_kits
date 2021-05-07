4a08e445e1666259e95ba6def614b26a5cb39074
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-07 16:05:44 +0200

    Fix factory to generate a kit price on the required product

--- lib/solidus_configurable_kits/testing_support/factories.rb
+++ lib/solidus_configurable_kits/testing_support/factories.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :kit_requirement, class: SolidusConfigurableKits::Requirement do
     product
     association :required_product, factory: :product
+
+    before(:create) do |kit_requirement|
+      kit_requirement.required_product.master.prices << ::Spree::Price.new(currency: "USD", amount: 0, kit_item: true)
+    end
   end
 end
