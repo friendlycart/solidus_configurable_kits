479c5a7c9567339f0f08dddea82608db4eed0a71
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-07 15:57:15 +0200

    Add PriceSelector#price_for
    Without this introduction, variants that are part of a bundle will show
    the wrong price on older Solidus versions.

--- app/services/solidus_configurable_kits/price_selector.rb
+++ app/services/solidus_configurable_kits/price_selector.rb
@@ -6,6 +6,19 @@ module SolidusConfigurableKits
       SolidusConfigurableKits::PricingOptions
     end
 
+    # The variant's price, given a set of pricing options
+    # @param [Spree::Variant::PricingOptions] price_options Pricing Options to abide by
+    # @return [Spree::Money, nil] The most specific price for this set of pricing options.
+    def price_for(price_options)
+      ::Spree::Deprecation.warn(
+        "price_for is deprecated and will be removed. The price_for method
+        should return a Spree::Price as described. Please use
+        #price_for_options and adjust your frontend code to explicitly call
+        &.money where required"
+      )
+      price_for_options(price_options)&.money
+    end
+
     # The variant's Spree::Price record, given a set of pricing options
     # @param [Spree::Variant::PricingOptions] price_options Pricing Options to abide by
     # @return [Spree::Price, nil] The most specific price for this set of pricing options.
