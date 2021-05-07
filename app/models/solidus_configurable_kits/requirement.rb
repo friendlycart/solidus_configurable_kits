c174df0e5283bd57eb032ea22bfa52eabffdf81a
Martin Meyerhoff <mamhoff@gmail.com>
2021-05-07 14:03:13 +0200

    Add validation to required product on kit_requirements
    A Product can only serve as a required product if it has a kit price.

--- app/models/solidus_configurable_kits/requirement.rb
+++ app/models/solidus_configurable_kits/requirement.rb
@@ -4,5 +4,15 @@ module SolidusConfigurableKits
   class Requirement < ::Spree::Base
     belongs_to :product, class_name: "Spree::Product", inverse_of: :kit_requirements
     belongs_to :required_product, class_name: "Spree::Product", inverse_of: :kits
+
+    validate :required_product_has_kit_item_prices
+
+    private
+
+    def required_product_has_kit_item_prices
+      if required_product&.prices&.where(kit_item: true)&.none?
+        errors.add(:required_product, :needs_at_least_one_variant_with_a_kit_price)
+      end
+    end
   end
 end
--- spec/models/solidus_configurable_kits/requirement_spec.rb
+++ spec/models/solidus_configurable_kits/requirement_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe SolidusConfigurableKits::Requirement do
   it { is_expected.to belong_to :required_product }
   it { is_expected.to respond_to :quantity }
   it { is_expected.to respond_to :description }
+
+  describe "validations" do 
+    subject(:requirement) { build(:kit_requirement, required_product: required_product) }
+    let(:required_product) { create(:product) }
+    let(:price) { create(:price, kit_item: true) }
+
+    before do 
+      required_product.master.prices << price
+    end
+
+    it { is_expected.to be_valid }
+
+    context "if there is no price for kit items on the required product" do 
+      let(:price) { create(:price, kit_item: false) }
+
+      it { is_expected.not_to be_valid }
+
+      it "has the right error message" do 
+        subject.valid?
+        expect(subject.errors.full_messages).to include(
+          "Required Product needs to have at least one variant with a kit price"
+        )
+      end
+    end
+  end
 end
