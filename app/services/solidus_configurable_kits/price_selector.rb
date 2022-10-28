ec50ed7b4b548f159a9ac6cf7be146bd104bced6
Martin Meyerhoff <mamhoff@gmail.com>
2022-10-28 13:03:33 +0200

    Use PriceSelecter#sorted_prices_for
    With recent changes in Solidus, we need to be using this private method
    rather than `Spree::Variant#currently_valid_prices`.

--- app/services/solidus_configurable_kits/price_selector.rb
+++ app/services/solidus_configurable_kits/price_selector.rb
@@ -23,7 +23,7 @@ module SolidusConfigurableKits
     # @param [Spree::Variant::PricingOptions] price_options Pricing Options to abide by
     # @return [Spree::Price, nil] The most specific price for this set of pricing options.
     def price_for_options(price_options)
-      variant.currently_valid_prices.detect do |price|
+      sorted_prices_for(variant).detect do |price|
         (price.country_iso == price_options.desired_attributes[:country_iso] ||
           price.country_iso.nil?
         ) && price.currency == price_options.desired_attributes[:currency] &&
