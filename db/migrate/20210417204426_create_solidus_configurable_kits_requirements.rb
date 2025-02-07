54877a22df1e1a3c6b96992637848ebc4b986e08
Martin Meyerhoff <mamhoff@gmail.com>
2025-02-07 21:41:47 +0100

    Fix migrations for MariaDB/MySQL

--- db/migrate/20210417204426_create_solidus_configurable_kits_requirements.rb
+++ db/migrate/20210417204426_create_solidus_configurable_kits_requirements.rb
@@ -1,9 +1,9 @@
 class CreateSolidusConfigurableKitsRequirements < ActiveRecord::Migration[5.2]
   def change
     create_table :solidus_configurable_kits_requirements do |t|
-      t.references :product, null: false, foreign_key: { to_table: :spree_products },
+      t.references :product, type: :integer, null: false, foreign_key: { to_table: :spree_products },
         index: { name: :index_solidus_kit_products }
-      t.references :required_product, null: false, foreign_key: { to_table: :spree_products },
+      t.references :required_product, type: :integer, null: false, foreign_key: { to_table: :spree_products },
         index: { name: :index_solidus_kit_requirements }
       t.string :name
       t.text :description
